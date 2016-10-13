2a92b9977510c7fa5ebb77c50197a4e1602ca060
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2016-10-13 13:55:28 +0200

    Course: - Introduction - Quick starts - Core conceps - Components - Templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {StocksComponent} from "./stocks.component";
 
 @Component({
+  // moduleId:module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular 2!';
 }
